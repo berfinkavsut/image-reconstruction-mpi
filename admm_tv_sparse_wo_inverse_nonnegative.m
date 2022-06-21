b4b0c57832cc436fab590726cde2b4a1847afdcb
Berfin Kavsut <berfinkavsut@gmail.com>
2022-06-21 15:30:20 +0200

    Update admm_tv_sparse_wo_inverse_nonnegative.m

--- admm_tv_sparse_wo_inverse_nonnegative.m
+++ admm_tv_sparse_wo_inverse_nonnegative.m
@@ -1,6 +1,5 @@
-function [history,res] = 
-admm_tv_sparse_wo_inverse_nonnegative(A,b,rho,mu,lambda,tau,step_length,MAX_ITER,varargin)
-%admm_tv_sparse(A,b,rho,mu,lambda,step_length,tolerance)
+function [history,res] = admm_tv_sparse_wo_inverse_nonnegative(A,b,rho,mu,lambda,tau,step_length,MAX_ITER,varargin)
+% admm_tv_sparse(A,b,rho,mu,lambda,step_length,tolerance)
 % ADMM Function with Total Variation and L1 norm regularization
 % performs ADMM to find a sparse solution to the TV/L2 problem. e:time
 % spent, res: resulted x vector.
@@ -8,7 +7,7 @@ admm_tv_sparse_wo_inverse_nonnegative(A,b,rho,mu,lambda,tau,step_length,MAX_ITER
 % Original problem:
 % minimize (mu/2)*||y - Mx||^2 + tau*||x||_1 + lambda*||x||_TV
 %
-38
+
 % With ADMM:
 % minimize (mu/2)*||y - Mx||^2 + tau*||v||_1 + lambda*||w||_2
 % such that v = x, w_i = D_i*x for i = 1...n_y
@@ -21,39 +20,45 @@ admm_tv_sparse_wo_inverse_nonnegative(A,b,rho,mu,lambda,tau,step_length,MAX_ITER
 % reference paper: "Combined Analysis-L1 and Total Variation ADMM with 
 % Applications to MEG Brain Imaging and Signal Reconstruction" by Rui Gao, 
 % Filip Tronarp, and Simo Särkkä
+
 ref_flag = false; % reference image does not exist in default.
 if nargin>8 %checks whether reference image is given as input.
  ref_flag = true;
  ref_im = varargin{1};
 end
+
 %Global constants and defaults
 ABSTOL = 1e-4;
 RELTOL = 1e-3;
 t_start = cputime;
 [~, num_pos] = size(A);
 grid_size = sqrt(num_pos);
-%% compute the difference matrix D_i for each i 
+
+%% Compute the difference matrix D_i for each i 
 % For the point (i,j), take the difference between (i+1,j) and (i,j+1)
-%horizontal difference matrix.
+% horizontal difference matrix.
 n = num_pos;
 e = ones(n,1);
 D_horizontal = spdiags([e -e],0:1,n,n);
+
 % horizontal finite difference can not be calculated for the pixels on the
 % right edge. So, make the operator zero for that pixels.
 %border_ind = grid_size * (1:grid_size);
 D_horizontal(1,:) = 0;
 D_horizontal(end,:) = 0;
+
 %vertical difference matrix.
 e = ones(n,1);
 D_vertical = spdiags([-1*e e],[0,grid_size],n,n);
+
 % vertical finite difference can not be calculated for the pixels on the
 % bottom edge. So, make the operator zero for that pixels.
 %border_ind = grid_size * (grid_size-1);
 D_vertical(:,1) = 0;
 D_vertical(:,end) = 0;
 D = [D_horizontal', D_vertical']';
+
 %% ADMM Steps
-39
 delta = 1/((rho + rho * normest(D)^2 + mu * norm(A,2)^2));
 x = zeros(num_pos,1);
 w = zeros(num_pos,1); %sparsity 
@@ -62,10 +67,10 @@ y1 = zeros(num_pos,1);
 y2 = zeros(2*num_pos,1); 
 y3 = zeros(num_pos,1);
 s =x;
+
 for i = 1:MAX_ITER
  %% x-update
- x = x - delta*(y1 + D'*y2 + y3)+delta*rho*(x-w)+delta*rho*D'*(D*x-z)+delta*mu*A'*(b-A*x) + 
-delta*rho*(x-s);
+ x = x - delta*(y1 + D'*y2 + y3)+delta*rho*(x-w)+delta*rho*D'*(D*x-z)+delta*mu*A'*(b-A*x) + delta*rho*(x-s);
  %% w-update
  w_old = w;
  e = x + y1/rho;
@@ -106,7 +111,6 @@ delta*rho*(x-s);
  tolerance_primal3 = sqrt(num_pos)*ABSTOL + RELTOL*max(norm(x), norm(s));
  tolerance_dual1 = sqrt(num_pos)*ABSTOL + RELTOL*norm(y2);
  tolerance_dual2 = sqrt(num_pos)*ABSTOL + RELTOL*norm(y1);
-40
  tolerance_dual3 = sqrt(num_pos)*ABSTOL + RELTOL*norm(y3);
  
  if ((residual_primal1 < tolerance_primal1) && (residual_dual1 < tolerance_dual1) && (residual_primal2 < 
@@ -114,10 +118,14 @@ tolerance_primal2) && (residual_dual2 < tolerance_dual2) && (residual_primal3 < 
 (residual_dual3 < tolerance_dual3))
  break;
  end
+ 
 end
+
 res = x;
 history.cpu_time = cputime-t_start;
+
 end
+
 function res = objective(A,b,x,mu,lambda,tau,D)
 %calculates objective least square function with TV and l1 regularization.
 res = (mu/2)*norm(A*x-b)^2 + lambda * sum(vecnorm(D*x,2,2))+ tau*norm(x,1);
